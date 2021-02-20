1db597302c0e203f5c56b883719202f93402a481
Martin Sione <sionemartin@hotmail.com>
2021-02-20 15:56:51 -0300

    Cleaned up avatar component, more reusable

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,9 @@
 import style from "styles/Avatar.module.css"
 
-export default function Avatar({src, text}) {
+export default function Avatar({ alt, src, title }) {
   return (
-    <div className={style.container}>
-      <img className={style.avatar} src={src} alt={text} title={text} />
-      <h4 className={style.text}>{text}</h4>
-    </div>
+    <>
+      <img className={style.avatar} alt={alt} src={src} title={title} />
+    </>
   )
 }
