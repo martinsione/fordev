41062af9d3c8383256920ef4adc654b745fcc92b
Martin Sione <sionemartin@hotmail.com>
2021-02-24 09:27:37 -0300

    autoredirect user to "/" if not logged

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react"
+import Head from "next/head"
 import Layout from "components/Layout"
+import Header from "components/Header"
+import Navbar from "components/Navbar"
 import "styles/reset.css"
 import "styles/global.css"
 
-function MyApp({ Component, pageProps }) {
+import useUser, { USER_STATE } from "hooks/useUser"
+import { useRouter } from "next/router"
+
+function MyApp({ Component, pageProps, title }) {
+  const router = useRouter()
+  const user = useUser()
+
+  useEffect(() => {
+    user === null && router.replace("/")
+  }, [user])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>{title}</Head>
+      <Layout>
+        {user && <Header title={title} />}
+        <Component {...pageProps} />
+        {user && <Navbar />}
+      </Layout>
+    </>
   )
 }
 
