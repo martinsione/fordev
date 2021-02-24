9396801bddfe4a306b17bfb296fc6b6ab083d9e1
Martin Sione <sionemartin@hotmail.com>
2021-02-24 09:28:50 -0300

    Stylized the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,27 @@
 import style from "styles/Navbar.module.css"
+import Link from "next/link"
+import Create from "components/Icons/Create"
+import Home from "components/Icons/Home"
+import Search from "components/Icons/Search"
 
 export default function Navbar() {
-  return <nav className={style.container}></nav>
+  return (
+    <nav className={style.container}>
+      <Link href="/compose/post">
+        <a>
+          <Create width={32} height={32} stroke="#09f" />
+        </a>
+      </Link>
+      <Link href="/home">
+        <a>
+          <Search width={32} height={32} stroke="#09f" />
+        </a>
+      </Link>
+      <Link href="/home">
+        <a>
+          <Home width={32} height={32} stroke="#09f" />
+        </a>
+      </Link>
+    </nav>
+  )
 }
