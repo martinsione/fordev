fd9ea24e2070dbd11389a8cc2c7b9aadc98528ed
Martin Sione <sionemartin@hotmail.com>
2021-02-25 11:18:34 -0300

    add dinamic linking to posts

--- components/Post.js
+++ components/Post.js
@@ -2,6 +2,7 @@ import Avatar from "components/Avatar"
 import style from "styles/Post.module.css"
 import useTimeAgo from "hooks/useTimeAgo"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function Post({
   avatar,
@@ -12,15 +13,27 @@ export default function Post({
   username,
 }) {
   const timeago = useTimeAgo(createdAt)
+
+  const router = useRouter()
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    router.push(`/status/${id}`)
+  }
+
   return (
-    <article className={style.container} key={id}>
+    <article className={style.container} onClick={handleClick}>
       <div className={style.avatar}>
         <Avatar alt={username} src={avatar} title={username} />
       </div>
       <div className={style.text}>
         <span>
           <h4 className={style.username}>{username}</h4>
-          <p className={style.createdAt}>{timeago}</p>
+          <Link href={`/status/${id}`}>
+            <a className={style.createdAt}>
+              <time>{timeago}</time>
+            </a>
+          </Link>
         </span>
         <p className={style.content}>{content}</p>
       </div>
