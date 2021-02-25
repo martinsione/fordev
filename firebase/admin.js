91298a00f85315f9456bc96f0b5dbc0f9ad437e3
Martin Sione <sionemartin@hotmail.com>
2021-02-25 14:29:08 -0300

    fixed up .env variables

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,9 +1,12 @@
 const admin = require("firebase-admin")
-const serviceAccount = require("../service-account.json")
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      project_id: "fordev-app",
+    }),
     databaseURL: "https://fordev-app.firebaseio.com",
   })
 }
